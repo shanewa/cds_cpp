3046753cad56bc7cb881c89d8f557c9bc8e8a504
shanewa <shane.wang@nokia-sbell.com>
2020-08-04 22:04:05 +0800

    define some classes

---
+++ include/config_handler.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+class config
+{
+    public:
+        void read(vector<string> &);
+        void load(vector<string> &);
+    private:
+};
---
+++ include/path_parser.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class path
+{
+    private:
+        string alias;
+        string origin_path;
+        string format_path;
+    public:
+        string get_alias();
+        string get_origin_path();
+        string get_format_path();
+        void win2linux();
+};
